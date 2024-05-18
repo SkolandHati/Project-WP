23f4ecaeabc039f517383efd3c148d7126bacc48
SkolandHati <vlad.star.01@inbox.ru>
2024-05-18 12:12:50 +0300

    update FilterItem

--- components/content/FilterItem.vue
+++ components/content/FilterItem.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="filter-item">
-    <h2 class="title-head">Most Recent Orders</h2>
+    <h2 class="title-head title_h2">Most Recent Orders</h2>
 
       <div class="user-status">
         <h3>Online Status</h3>
@@ -28,18 +28,24 @@
 
 <script setup lang="ts">
 
+  let status = true
+
 </script>
 
 <style scoped lang="scss">
 
+  @import "assets/saas/media/desktop";
+
+  @include desktop;
+
   .filter-item{
-    background-color: #454444;
+    background-color: #313142;
     display: grid;
     min-width: 100%;
-    margin: 20px -10px 0 -10px;
+    margin: 20px -12px 0 -12px;
     padding-bottom: 15px;
     gap: 15px;
-    color: white;
+    color: #ffffff;
 
     .title-head{
       text-align: center;
@@ -57,39 +63,67 @@
         grid-template-rows: 15px 20px;
         align-items: center;
         gap: 15px;
+
+        span:first-child{
+          background-color: #63b88d;
+        }
+
         span{
           position: relative;
           display: grid;
           grid-template-columns: 10px auto;
           justify-content: center;
           text-align: center;
+          line-height: 30px;
           width: 80%;
+          gap: 10px;
           margin: auto;
           margin-top: 10px;
           max-width: 200px;
           height: 35px;
-          background-color: white;
+          background-color: #75d6e6;
           color: black;
-          line-height: 30px;
-          gap: 10px;
+          transition: all 500ms ease;
+          border: 1px solid transparent;
+
+          &:first-child:hover{
+            cursor: pointer;
+            transition: all 500ms ease;
+            background-color: rgb(103, 85, 243);
+            border: 1px solid #5e589c;
+          }
 
           &:hover{
             cursor: pointer;
-          }
+            transition: all 500ms ease;
+            background-color: #1280d6;
+            border: 1px solid #55a3ec;
 
-          &::before{
-            margin-top: 8px;
-            content: "";
-            float: right;
-            width: 10px;
-            height: 10px;
-            color: #ff0000;
-            border: 2px solid red;
-            border-radius: 50%;
+            &::before{
+              content: "";
+              margin-top: 9px;
+              float: right;
+              width: 10px;
+              height: 10px;
+              color: #545497;
+              border: 2px solid #545497;
+              background-color: black;
+              border-radius: 50%;
+            }
           }
+        &::before{
+          content: "";
+          margin-top: 9px;
+          float: right;
+          width: 10px;
+          height: 10px;
+          color: #000000;
+          border: 2px solid #000000;
+          border-radius: 50%;
         }
       }
     }
   }
+}
 
 </style>
